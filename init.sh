50fa7d3190cfc3c0fe7061ba7a5f525348d30a09
Gilberto Catarino <gilberto.catarino@gojira.tv>
2019-08-08 19:54:31 +0100

    Added script for initializing package branches

---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# This script will create a branch for each package
+
+declare -a packages=(
+                "com.stackray.spriterenderer"
+                "com.stackray.entities" 
+                "com.stackray.mathematics"
+                "com.stackray.collections"
+                "com.stackray.jobs"
+                "com.stackray.transforms"
+                )
+
+for package in "${packages[@]}"
+do
+  git checkout --orphan $package
+  git rm -rf .
+  git checkout master Packages/$package ./Packages/$package
+  mv Packages/$package/*.* .
+  rm -r Packages
+  git add .
+  git commit -a -m"$package Package"
+  git push --set-upstream origin $package
+done
+
+git checkout master
+
+
