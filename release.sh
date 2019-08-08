b069d9c09b1c12306073ae0a42d21810ece1ab99
Gilberto Catarino <gilberto.catarino@gojira.tv>
2019-08-08 18:58:31 +0100

    Added release script

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# This script will create a new version of all stackray packages, providing  the version as parameter
+# NOTE: the script will go to `master` if not already there.
+
+declare -a packages=(
+                "com.stackray.spriterenderer"
+                "com.stackray.entities" 
+                "com.stackray.mathematics"
+                "com.stackray.collections"
+                "com.stackray.jobs"
+                "com.stackray.transforms"
+                )
+                
+for package in "${packages[@]}"
+do
+  git checkout $package
+  git checkout master Packages/$package .
+  mv Packages/$package/*.* .
+  rm -r Packages
+  git push
+  git tag $package-$1
+  git push origin $package-$1
+done
+
+git checkout master
+
+
