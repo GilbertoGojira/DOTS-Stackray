2d5c3f53f23204ac18b5cdb8e61f6fb470dfa7d8
Gilberto Catarino <gilberto.catarino@gojira.tv>
2019-08-08 22:44:05 +0000

    packages to version

--- Packages/com.stackray.spriterenderer/Stackray.SpriteRenderer/Components/LocalToWorldProperty.cs
+++
@@ -1,7 +0,0 @@
-﻿using Unity.Transforms;
-
-namespace Stackray.SpriteRenderer {
-  public struct LocalToWorldProperty : IFixedBufferProperty<LocalToWorld> {
-    public string BufferName => "localToWorldBuffer";
-  }
-}
--- Packages/com.stackray.spriterenderer/Stackray.SpriteRenderer/Components/SpriteBounds.cs
+++
@@ -1,9 +0,0 @@
-﻿using Unity.Entities;
-using Unity.Mathematics;
-
-namespace Stackray.SpriteRenderer {
-  public struct SpriteBounds : IComponentData {
-    public AABB Value;
-  }
-}
-
