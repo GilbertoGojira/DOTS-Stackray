a170c0d8bd0c6951fcb73d16b880f000de73eeee
Gilberto Catarino <gilberto.catarino@gojira.tv>
2019-09-24 00:57:48 +0100

    Update release script to check for branch existence and to include Text package

--- release.sh
+++ release.sh
@@ -16,10 +16,23 @@ declare -a packages=(
                 "com.stackray.collections"
                 "com.stackray.jobs"
                 "com.stackray.transforms"
+                "com.stackray.text"
                 )
                 
 for package in "${packages[@]}"
 do
+  branch_exist=$(git show-ref refs/heads/$package) 
+  if [ -z "$branch_exist" ]
+  then
+    git checkout --orphan $package
+    git rm -rf .
+    git checkout master Packages/$package ./Packages/$package
+    mv Packages/$package/*.* .
+    rm -r Packages
+    git add .
+    git commit -a -m"$package Package"
+    git push --set-upstream origin $package
+  fi 
   git checkout $package
   git checkout master Packages/$package ./Packages/$package
   cp -rlf Packages/$package/*.* .
@@ -28,6 +41,15 @@ do
   git push
   git tag $package-$1
   git push origin $package-$1
+  # always create the 'latest' tag pointing to latest version
+  latest_exist=$(git show-ref refs/heads/$package-latest)
+  if [ -z "$latest_exist" ]
+  then
+    git push --delete origin $package-latest
+    git tag -d $package-latest
+  fi
+  git tag $package-latest
+  git push origin $package-latest
 done
 
 git checkout master
