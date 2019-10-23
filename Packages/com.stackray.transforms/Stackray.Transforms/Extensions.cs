f980d93eab102a7a12d43ebe23e15168ef9d0d75
Gilberto Catarino <gilberto.catarino@gojira.tv>
2019-10-23 01:53:19 +0100

    Add extension to get global array of all entities sorted by distance to camera TODO: Make it generic

---
+++ Packages/com.stackray.transforms/Stackray.Transforms/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+
+namespace Stackray.Transforms {
+  public static class Extensions {
+    public static NativeArray<SortedEntity> GetAllSortedEntities(this EntityManager entityManager, ComponentSystemBase system, Allocator allocator) {
+      return entityManager.GetBuffer<SortedEntity>(system.GetSingletonEntity<SortedEntities>()).ToNativeArray(allocator);
+    }
+  }
+}
