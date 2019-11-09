3998c359677ceabca418d4f9d7c14ede84ea24a7
Gilberto Catarino <gilberto.catarino@gojira.tv>
2019-11-09 16:22:58 +0000

    Added script order attribute for update the script execution order without the editor tools

---
+++ Packages/com.stackray.entities/Stackray.Entities.Hybrid/ScriptOrderManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEditor;
+
+namespace Stackray.Entities {
+
+  public class ScriptOrder : Attribute {
+    public int order;
+
+    public ScriptOrder(int order) {
+      this.order = order;
+    }
+  }
+
+  [InitializeOnLoad]
+  public class ScriptOrderManager {
+
+    static ScriptOrderManager() {
+      foreach (var monoScript in MonoImporter.GetAllRuntimeMonoScripts()) {
+        if (monoScript.GetClass() != null)
+          foreach (var a in Attribute.GetCustomAttributes(monoScript.GetClass(), typeof(ScriptOrder))) {
+            var currentOrder = MonoImporter.GetExecutionOrder(monoScript);
+            var newOrder = ((ScriptOrder)a).order;
+            if (currentOrder != newOrder)
+              MonoImporter.SetExecutionOrder(monoScript, newOrder);
+          }
+      }
+    }
+  }
+}
