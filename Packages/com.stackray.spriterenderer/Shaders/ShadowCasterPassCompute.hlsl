9d532c34f00ce4bf12187a96a75529fa140d9495
Gilberto Catarino <gilberto.catarino@gojira.tv>
2019-11-23 16:32:41 +0000

    Added version of Lit shader with compute buffers for positioning and color

---
+++ Packages/com.stackray.spriterenderer/Shaders/ShadowCasterPassCompute.hlsl
@@ -0,0 +1,63 @@
+#ifndef UNIVERSAL_SHADOW_CASTER_PASS_INCLUDED
+#define UNIVERSAL_SHADOW_CASTER_PASS_INCLUDED
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Shadows.hlsl"
+#include "MathConvert.hlsl"
+#include "StructuredBuffers.hlsl"
+
+float3 _LightDirection;
+
+struct Attributes
+{
+    float4 positionOS   : POSITION;
+    float3 normalOS     : NORMAL;
+    float2 texcoord     : TEXCOORD0;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct Varyings
+{
+    float2 uv           : TEXCOORD0;
+    float4 positionCS   : SV_POSITION;
+};
+
+float4 GetShadowPositionHClip(Attributes input)
+{
+    float3 positionWS = TransformObjectToWorld(input.positionOS.xyz);
+    float3 normalWS = TransformObjectToWorldNormal(input.normalOS);
+
+    float4 positionCS = TransformWorldToHClip(ApplyShadowBias(positionWS, normalWS, _LightDirection));
+
+#if UNITY_REVERSED_Z
+    positionCS.z = min(positionCS.z, positionCS.w * UNITY_NEAR_CLIP_VALUE);
+#else
+    positionCS.z = max(positionCS.z, positionCS.w * UNITY_NEAR_CLIP_VALUE);
+#endif
+
+    return positionCS;
+}
+
+Varyings ShadowPassVertex(Attributes input, uint instanceID : SV_InstanceID)
+{
+#ifdef USE_COMPUTE
+    float4x4 localToWorld = uint4x2ToFloat4x4(localToWorldBuffer[instanceID]);
+    input.positionOS = mul(localToWorld, input.positionOS);
+    input.normalOS = mul(localToWorld, float4(input.normalOS.x, input.normalOS.y, input.normalOS.z, 0)).xyz;
+#endif
+
+    Varyings output;
+    UNITY_SETUP_INSTANCE_ID(input);
+
+    output.uv = TRANSFORM_TEX(input.texcoord, _BaseMap);
+    output.positionCS = GetShadowPositionHClip(input);
+    return output;
+}
+
+half4 ShadowPassFragment(Varyings input) : SV_TARGET
+{
+    Alpha(SampleAlbedoAlpha(input.uv, TEXTURE2D_ARGS(_BaseMap, sampler_BaseMap)).a, _BaseColor, _Cutoff);
+    return 0;
+}
+
+#endif
