dced2b2aea1d53800919432e2d40d99fee61b7fd
Gilberto Catarino <gilberto.catarino@gojira.tv>
2019-12-03 16:32:54 +0000

    Randomize colors per frame

--- SpriteRendererExample/Assets/Systems/RandomColorSystem.cs
+++ SpriteRendererExample/Assets/Systems/RandomColorSystem.cs
@@ -12,14 +12,14 @@ public class RandomColorSystem : JobComponentSystem {
   struct RandomColor : IJobForEachWithEntity<ColorProperty> {
     public uint RandomSeed;
     public void Execute(Entity entity, int index,[WriteOnly]ref ColorProperty c0) {
-      var random = new Random(RandomSeed + (uint)index);
+      var random = new Random(1 + RandomSeed + (uint)index);
       c0.Value = new half4((half3)random.NextFloat3(), (half)1);
     }
   }
 
   protected override JobHandle OnUpdate(JobHandle inputDeps) {
     return new RandomColor {
-      RandomSeed = (uint)System.DateTime.Now.Second
+      RandomSeed = (uint)System.DateTime.Now.Millisecond
     }.Schedule(this, inputDeps);
   }
 }
