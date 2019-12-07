144ed47cd23e6533af3dce77f5b684024be90e51
Gilberto Catarino <gilberto.catarino@gojira.tv>
2019-12-07 04:35:20 +0000

    Added Entity component inspector for supporting drawing custom inspectors for entities in the entity debugger

---
+++ Packages/com.stackray.entities/Stackray.Entities.Editor/IComponentEditor.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+
+namespace Stackray.Entities.Editor {
+
+  public interface IComponentEditor<T> {
+    void OnInspectorGUI(T target);
+  }
+
+  public interface IComponentDataEditor<T> : IComponentEditor<T> 
+    where T : struct, IComponentData {  }
+
+  public interface ISharedComponentDataEditor<T> : IComponentEditor<T>
+    where T : struct, ISharedComponentData {  }
+
+  public interface IBufferElementDataEditor<T> : IComponentEditor<T> 
+    where T : struct, IBufferElementData {  }
+}
