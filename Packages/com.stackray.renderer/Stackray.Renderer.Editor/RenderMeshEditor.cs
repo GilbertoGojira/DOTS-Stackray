f57d1a34e0052e93acfb0ca461247e32abc678dc
Gilberto Catarino <gilberto.catarino@gojira.tv>
2019-12-07 04:36:54 +0000

    Added custom component inspector for RenderMesh

---
+++ Packages/com.stackray.renderer/Stackray.Renderer.Editor/RenderMeshEditor.cs
@@ -0,0 +1,27 @@
+﻿using Stackray.Entities.Editor;
+using Unity.Rendering;
+using UnityEditor;
+using UnityEngine;
+
+namespace Stackray.Renderer {
+  public class RenderMeshEditor : ISharedComponentDataEditor<RenderMesh> {
+    public void OnInspectorGUI(RenderMesh target) {
+      var enabled = GUI.enabled;
+      GUI.enabled = true;
+      EditorGUILayout.LabelField(
+        nameof(RenderMesh),
+        new GUIStyle(EditorStyles.boldLabel) {
+          fontStyle = FontStyle.Bold
+        });
+      GUI.enabled = enabled;
+      EditorGUI.indentLevel++;
+      EditorGUILayout.ObjectField(nameof(RenderMesh.mesh), target.mesh, typeof(Mesh), false);
+      EditorGUILayout.ObjectField(nameof(RenderMesh.material), target.material, typeof(Material), false);
+      EditorGUILayout.IntField(nameof(RenderMesh.subMesh), target.subMesh);
+      EditorGUILayout.IntField(nameof(RenderMesh.layer), target.layer);
+      EditorGUILayout.EnumFlagsField(nameof(RenderMesh.castShadows), target.castShadows);
+      EditorGUILayout.Toggle(nameof(RenderMesh.receiveShadows), target.receiveShadows);
+      EditorGUI.indentLevel--;
+    }
+  }
+}
