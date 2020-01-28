e6fc2da2bb8395c38388421b24f18316eae7a2ba
Gilberto Catarino <gilberto.catarino@gojira.tv>
2020-01-28 23:43:23 +0000

    Updated packages to version 0.1.0-preview.1

---
+++ ActionJob.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using Unity.Burst;
+using Unity.Jobs;
+
+namespace Stackray.Jobs {
+  public delegate void Action();
+
+  /// <summary>
+  /// This job will execute an action
+  /// </summary>
+  [BurstCompile]
+  public struct ActionJob : IJob {
+
+    public static JobHandle Schedule(Action action, JobHandle inputDeps) {
+      return new ActionJob {
+        Action = new FunctionPointer<Action>(Marshal.GetFunctionPointerForDelegate(action))
+      }.Schedule(inputDeps);
+    }
+
+    FunctionPointer<Action> Action;
+
+    public void Execute() {
+      Action.Invoke();
+    }
+  }
+}
