2f5e0a2b975be4fd8e4807b046630c64ce5460ae
Gilberto Catarino <gilberto.catarino@gojira.tv>
2020-01-28 23:43:58 +0000

    Updated packages to version 0.1.0-preview.1

---
+++ Stackray.Text/FontMaterial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Stackray.Text {
+  public struct FontMaterial : ISharedComponentData, IEquatable<FontMaterial> {
+    public Material Value;
+
+    public bool Equals(FontMaterial other) {
+      return Equals(Value, other.Value);
+    }
+
+    public override bool Equals(object obj) {
+      if (ReferenceEquals(null, obj)) return false;
+      return obj is FontMaterial other && Equals(other);
+    }
+
+    public override int GetHashCode() {
+      return (Value != null ? Value.GetHashCode() : 0);
+    }
+  }
+}
---
+++ Stackray.Text/LimitActiveTextSystem.cs
@@ -0,0 +1,9 @@
+﻿using Stackray.Text;
+using Stackray.Transforms;
+using Unity.Entities;
+
+[assembly: RegisterGenericComponentType(typeof(LimitActiveComponentSystem<TextRenderer>.LimitActive<TextRenderer>))]
+
+namespace Stackray.Text {
+  public class LimitActiveTextSystem : LimitActiveComponentSystem<TextRenderer> { }
+}
