3f50fcf3337be7bdefd6c8c73480502a9f731789
Gilberto Catarino <gilberto.catarino@gojira.tv>
2020-04-27 21:17:18 +0100

    Added example stress test scene using unity hybrid renderer and matrial color

---
+++ SpriteRendererExample/Assets/Components/UseUnityRenderer.cs
@@ -0,0 +1,16 @@
+﻿using Stackray.Renderer;
+using Unity.Entities;
+using Unity.Rendering;
+using UnityEngine;
+
+public class UseUnityRenderer : MonoBehaviour {
+  private void Awake() {
+    World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<RendererSystem>().Enabled = false;
+
+#if ENABLE_HYBRID_RENDERER_V2
+    World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<HybridRendererSystem>().Enabled = true;
+#else
+    World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<RenderMeshSystemV2>().Enabled = true;
+#endif
+  }
+}
--- SpriteRendererExample/Assets/Systems/RandomColorSystem.cs
+++ SpriteRendererExample/Assets/Systems/RandomColorSystem.cs
@@ -1,17 +1,25 @@
 ﻿using Stackray.Renderer;
-using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
+using Unity.Rendering;
 
 public class RandomColorSystem : SystemBase {
+
   protected override void OnUpdate() {
     var seed = (uint)System.DateTime.Now.Millisecond + 1;
     Entities
       .WithAll<RandomColorTag>()
       .ForEach((Entity entity, int entityInQueryIndex, ref ColorProperty colorProp) => {
-      var random = new Random(seed + (uint)entityInQueryIndex);
-      colorProp.Value = new half4((half3)random.NextFloat3(), (half)1);
-    }).ScheduleParallel();
+        var random = new Random(seed + (uint)entityInQueryIndex);
+        colorProp.Value = new half4((half3)random.NextFloat3(), (half)1);
+      }).ScheduleParallel();
+
+    Entities
+    .WithAll<RandomColorTag>()
+        .ForEach((Entity entity, int entityInQueryIndex, ref MaterialColor colorProp) => {
+          var random = new Random(seed + (uint)entityInQueryIndex);
+          colorProp.Value = new float4(random.NextFloat3(), 1);
+        }).ScheduleParallel();
   }
 }
